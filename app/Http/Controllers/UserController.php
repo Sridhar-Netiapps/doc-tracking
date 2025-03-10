bd72b1c00d625d3cf23f21d8b10692a504948034
Sridhar-Netiapps <sridhar@netiapps.com>
2025-03-10 18:24:58 +0530

    New Changes Added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,10 +11,10 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('permission:view-user')->only('index','show');
-        $this->middleware('permission:create-user')->only(['create', 'store']);
-        $this->middleware('permission:edit-user')->only(['edit', 'update']);
-        $this->middleware('permission:delete-user')->only('destroy');
+        // $this->middleware('permission:view-user')->only('index','show');
+        // $this->middleware('permission:create-user')->only(['create', 'store']);
+        // $this->middleware('permission:edit-user')->only(['edit', 'update']);
+        // $this->middleware('permission:delete-user')->only('destroy');
     }
 
     // List all users
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('roles', RoleController::class);
+Route::resource('permissions', PermissionController::class);
+
+Route::post('users/{user}/roles', [UserController::class, 'assignRole'])->name('users.assignRole');
+Route::post('users/{user}/permissions', [UserController::class, 'assignPermission'])->name('users.assignPermission');
