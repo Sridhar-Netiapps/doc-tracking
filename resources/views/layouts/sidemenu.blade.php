2e42639abc867aa1fea36fad12c149acfe82a456
Sridhar-Netiapps <sridhar@netiapps.com>
2025-03-18 17:21:14 +0530

    roles admin panel added

---
+++ resources/views/layouts/sidemenu.blade.php
@@ -0,0 +1,9 @@
+<div class="col-2">
+    <div class="leftPanel">
+        <div class="treeList">
+            <ul>
+                <li><a href="{{route('process_status.index')}}"><span class="material-symbols-outlined">folder</span> Groups <span>30</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
