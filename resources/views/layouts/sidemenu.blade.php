c13e56893b367a5063c88525b035ec7a3c9e869e
shashi <shashi09568@gmail.com>
2025-03-19 18:19:49 +0530

    login page design

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,12 +1,12 @@
 
         <div class="treeList h-100">
             <ul>
-                <li class="mb-auto"><a href="/">Back to Dashboard</a></li>
-                <li><a href="">Group</a></li>
-                <li><a href="/">Users</a></li>
-                <li><a href="/">Roles</a></li>
-                <li><a href="/">Permission</a></li>
-                <li class="mt-auto"><a href="/">Settings</a></li>
+                <li class="mb-auto"><a href="/"><img src="/images/material-symbols-light--dashboard-outline-rounded.svg" /> Back to Dashboard</a></li>
+                <li><a href="/"><img src="/images/material-symbols-light--folder-supervised-outline.svg" /> Group</a></li>
+                <li><a href="/"><img src="/images/material-symbols-light--group-add.svg"/> Users</a></li>
+                <li><a href="/"><img src="/images/material-symbols-light--folder-supervised-outline.svg" /> Roles</a></li>
+                <li><a href="/"><img src="/images/material-symbols-light--lock-person-outline-rounded.svg"/> Permission</a></li>
+                <li class="mt-auto"><a href="/"><img src="/images/material-symbols-light--settings-outline.svg" /> Settings</a></li>
             </ul>
         </div>
 
