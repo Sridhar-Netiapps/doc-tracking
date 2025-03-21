7773f527845f0b5724e01bfca86545c6cfa31746
shashi <shashi09568@gmail.com>
2025-03-21 19:06:21 +0530

    login page design

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -7,7 +7,7 @@
                 <li><a href="{{ route('roles.index') }}"><img src="/images/material-symbols-light--folder-supervised-outline.svg" /> Roles</a></li>
                 <li><a href="{{ route('process_status.index') }}"><img src="/images/material-symbols-light--folder-supervised-outline.svg" /> Process Status</a></li>
                 <li><a href="{{ route('permissions.index') }}"><img src="/images/material-symbols-light--lock-person-outline-rounded.svg"/> Permission</a></li>
-                <li class="mt-auto"><a href="/"><img src="/images/material-symbols-light--settings-outline.svg" /> Settings</a></li>
+                <li class="mt-auto active rotateAni"><a href="/"><img src="/images/material-symbols-light--settings-outline.svg" /> Settings</a></li>
             </ul>
         </div>
 
