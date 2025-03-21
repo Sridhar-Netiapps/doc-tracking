97040398eb66c2d55d77165edc794a110a24dcde
shashi <shashi09568@gmail.com>
2025-03-21 16:18:11 +0530

    login page design

--- resources/views/process_status/edit.blade.php
+++ resources/views/process_status/edit.blade.php
@@ -2,9 +2,24 @@
 
 @section('content')
 <div class="rightPanel">
-    <div class="d-flex justify-content-between align-items-center mb-2">
-        <h3>Edit Process Status</h3>
+    <div class="d-flex justify-content-between align-items-center mb-2 headerTitle">
+        <div>
+            <div class="d-flex justify-content-center align-items-center">
+                <h3 class="me-3">Edit Process Status</h3>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Library</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data</li>
+                    </ol>
+                </nav>
+            </div>
+
+        </div>
     </div>
+
+
+
 
     @if (session('success'))
         <div class="alert alert-success mt-3">
@@ -12,39 +27,56 @@
         </div>
     @endif
 
-    <form action="{{ route('process_status.update', $status->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+    <div class="row">
+        <div class="col-6">
+            <div class="form-card">
+                <form action="{{ route('process_status.update', $status->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
 
-        <div class="mb-4">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" name="name" class="form-control" id="name" required pattern="^[a-zA-Z\s]+$" maxlength="55" value="{{ old('name', $status->name) }}">
+                    <div class="mb-4">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" id="name" required pattern="^[a-zA-Z\s]+$" maxlength="55" value="{{ old('name', $status->name) }}">
+                    </div>
+
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="mb-4">
+                                <label for="status" class="form-label">Status</label>
+                                <select name="status" class="form-select" required>
+                                    <option value="1" {{ $status->status == 1 ? 'selected' : '' }}>Active</option>
+                                    <option value="0" {{ $status->status == 0 ? 'selected' : '' }}>Inactive</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="mb-4">
+                                <label for="updated_by" class="form-label">Updated By</label>
+                                <select name="updated_by" class="form-select" required>
+                                    <option value="1" {{ $status->updated_by == 1 ? 'selected' : '' }}>Person 1</option>
+                                    <option value="2" {{ $status->updated_by == 2 ? 'selected' : '' }}>Person 2</option>
+                                </select>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+
+
+                    <div class="d-flex ">
+                        <!-- Save Button -->
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <!-- Cancel Button to Redirect to Process Status Index -->
+                        <a href="{{ route('process_status.index') }}" class="btn btn-secondary ms-3">Cancel</a>
+                    </div>
+
+                </form>
+
+            </div>
         </div>
+    </div>
 
-        <div class="mb-4">
-            <label for="status" class="form-label">Status:</label>
-            <select name="status" class="form-control" required>
-                <option value="1" {{ $status->status == 1 ? 'selected' : '' }}>Active</option>
-                <option value="0" {{ $status->status == 0 ? 'selected' : '' }}>Inactive</option>
-            </select>
-        </div>
-
-        <div class="mb-4">
-            <label for="updated_by" class="form-label">Updated By:</label>
-            <select name="updated_by" class="form-control" required>
-                <option value="1" {{ $status->updated_by == 1 ? 'selected' : '' }}>Person 1</option>
-                <option value="2" {{ $status->updated_by == 2 ? 'selected' : '' }}>Person 2</option>
-            </select>
-        </div>
-
-            <div class="d-flex ">
-            <!-- Save Button -->
-            <button type="submit" class="btn btn-primary">Save</button>
-
-            <!-- Cancel Button to Redirect to Process Status Index -->
-            <a href="{{ route('process_status.index') }}" class="btn btn-secondary ml-2">Cancel</a>
-        </div>
-
-    </form>
 </div>
 @endsection
