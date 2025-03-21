cb3bc95dd1f1306081847e0f50800bf434f6c549
shashi <shashi09568@gmail.com>
2025-03-21 17:26:14 +0530

    login page design

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,7 +20,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-top">
             <div class="container">
                 <a class="navbar-brand border-0" href="{{ url('/') }}">
                     <img src="/images/logo1.svg" />
@@ -73,15 +73,15 @@
                 </div>
             </div>
         </nav>
-        <main class="">
+        <main class="page-container">
             <div class="row h-100 px-0">
-                <div class="col-2 bg-white h-100 px-0">
+                <div class="col-2 bg-white h-100 px-0 position-fixed">
                     <div class="leftMenu">
                         @include('layouts.sidemenu')
                     </div>
 
                 </div>
-                <div class="col-10 px-0">
+                <div class="col-10 px-0 pushLeft">
                     @yield('content')
                 </div>
             </div>
