e12fc007514eecf2d704cb27965bff47f3e276ca
shashi <shashi09568@gmail.com>
2025-03-24 16:46:30 +0530

    login page design

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,9 +2,25 @@
 
 @section('content')
 <div class="rightPanel">
-    <div class="d-flex justify-content-between align-items-center mb-2">
-        <h3>Edit User</h3>
+
+
+    <div class="d-flex justify-content-between align-items-center mb-2 headerTitle">
+        <div>
+            <div class="d-flex justify-content-center align-items-center">
+                <h3 class="me-3">Edit User</h3>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Library</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data</li>
+                    </ol>
+                </nav>
+            </div>
+
+        </div>
     </div>
+
+
 
     @if (session('success'))
         <div class="alert alert-success mt-3">
@@ -12,46 +28,55 @@
         </div>
     @endif
 
-    <form action="{{ route('users.update', $user->id) }}" method="POST">
-        @csrf
-        @method('PUT')
 
-        <div class="mb-4">
-            <label for="name" class="form-label">Name:</label>
-            <input type="text" name="name" class="form-control" id="name" required maxlength="255" value="{{ old('name', $user->name) }}">
-        </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="form-card">
+                <form action="{{ route('users.update', $user->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
 
-        <div class="mb-4">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" name="email" class="form-control" id="email" required value="{{ old('email', $user->email) }}">
-        </div>
+                    <div class="mb-4">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" class="form-control" id="name" required maxlength="255" value="{{ old('name', $user->name) }}">
+                    </div>
 
-        <div class="mb-4">
-            <label for="roles" class="form-label">Roles:</label>
-            <select name="roles[]" class="form-control" id="roles" multiple required>
-                @foreach ($roles as $role)
-                    <option value="{{ $role->name }}" {{ $user->hasRole($role->name) ? 'selected' : '' }}>
-                        {{ $role->name }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
+                    <div class="mb-4">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="email" required value="{{ old('email', $user->email) }}">
+                    </div>
 
-        <div class="mb-4">
-            <label for="permissions" class="form-label">Permissions:</label>
-            <select name="permissions[]" class="form-control" id="permissions" multiple required>
-                @foreach ($permissions as $permission)
-                    <option value="{{ $permission->name }}" {{ $user->hasPermissionTo($permission->name) ? 'selected' : '' }}>
-                        {{ $permission->name }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
+                    <div class="mb-4">
+                        <label for="roles" class="form-label">Roles</label>
+                        <select name="roles[]" class="form-control" id="roles" multiple required>
+                            @foreach ($roles as $role)
+                                <option value="{{ $role->name }}" {{ $user->hasRole($role->name) ? 'selected' : '' }}>
+                                    {{ $role->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
 
-        <div class="form-group d-flex justify-content-between">
-            <button type="submit" class="btn btn-primary">Save</button>
-            <a href="{{ route('users.index') }}" class="btn btn-secondary">Cancel</a>
+                    <div class="mb-4">
+                        <label for="permissions" class="form-label">Permissions</label>
+                        <select name="permissions[]" class="form-control" id="permissions" multiple required>
+                            @foreach ($permissions as $permission)
+                                <option value="{{ $permission->name }}" {{ $user->hasPermissionTo($permission->name) ? 'selected' : '' }}>
+                                    {{ $permission->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary me-3">Save</button>
+                        <a href="{{ route('users.index') }}" class="btn btn-outline-dark">Cancel</a>
+                    </div>
+                </form>
+            </div>
         </div>
-    </form>
+    </div>
+
+
 </div>
 @endsection
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -14,57 +14,62 @@
     @endif
 
     <div class="row">
-        <div class="table-responsive">
-            <table class="table table-striped table-bordered align-middle">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Roles</th>
-                        <!-- <th>Permissions</th> --> 
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
+        <div class="col-12">
+            <div class="form-card">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered align-middle">
+                        <thead>
                         <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>
-                                @foreach ($user->roles as $role)
-                                    <span class="badge bg-info">{{ $role->name }}</span>
-                                @endforeach
-                            </td>
-                            <!-- <td>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Roles</th>
+                            <!-- <th>Permissions</th> -->
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    @foreach ($user->roles as $role)
+                                        <span class="badge bg-danger">{{ $role->name }}</span>
+                                    @endforeach
+                                </td>
+                                <!-- <td>
                                 @foreach ($user->permissions as $permission)
                                     <span class="badge bg-primary">{{ $permission->name }}</span>
                                 @endforeach
-                            </td> -->
-                            <td>
-                                <div class="btn-actions">
-                                    <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Edit</a>
-                                    
-                                    <!-- Delete User -->
-                                    <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
-                                    </form>
-                                    
-                                    <!-- Assign Role -->
-                                    <!-- <a href="{{ route('users.assignRole', $user->id) }}" class="btn btn-warning btn-sm">Assign Role</a> -->
-                                    
-                                    <!-- Assign Permission -->
-                                    <!-- <a href="{{ route('users.assignPermission', $user->id) }}" class="btn btn-success btn-sm">Assign Permission</a> -->
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                </td> -->
+                                <td>
+                                    <div class="btn-actions">
+                                        <a href="{{ route('users.edit', $user->id) }}" class="btn btn-primary btn-sm">Edit</a>
+
+                                        <!-- Delete User -->
+                                        <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
+                                        </form>
+
+                                        <!-- Assign Role -->
+                                        <!-- <a href="{{ route('users.assignRole', $user->id) }}" class="btn btn-warning btn-sm">Assign Role</a> -->
+
+                                        <!-- Assign Permission -->
+                                        <!-- <a href="{{ route('users.assignPermission', $user->id) }}" class="btn btn-success btn-sm">Assign Permission</a> -->
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
+
     </div>
 </div>
 @endsection
