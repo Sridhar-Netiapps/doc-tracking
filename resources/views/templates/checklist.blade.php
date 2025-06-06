4f8c6a7ef59c8475c48dd099a53b1b217c15e722
druva <druva@netiapps.com>
2025-06-06 19:05:42 +0530

    checklist form

--- resources/views/templates/checklist.blade.php
+++ resources/views/templates/checklist.blade.php
@@ -20,7 +20,7 @@
   margin: 5px;
  }
 .contentmain{
-  margin: 20px;
+  margin: 10px 20px 10px 20px;
   padding: 5px;
   border: 1px solid black;
 }
@@ -32,8 +32,6 @@
 .twocol-table {
   width: 100%;
   border-collapse: collapse;
-  margin-top: 10px;
-  margin-bottom: 10px;
  
 }
 .twocol-table td, .twocol-table th {
@@ -85,8 +83,18 @@
 .col-8 {
   width: 7%;
 }
+
 .col-65 {
   width: 65%;
+}
+.col-52 {
+  width: 53%;
+}
+.col-92 {
+  width: 93%;
+}
+.col-32 {
+  width: 32%;
 }
 .label-font-bold{
 	font-size: 15px;
@@ -135,6 +143,16 @@
 	display: inline-block;
 	border-bottom: 1px solid black;
 }
+.lableslno{
+	text-align: center;
+	vertical-align: middle;
+}
+.td-bg{
+   background-color: #d2d3d2;
+}
+.margintop{
+	margin-top: 10px;
+}
 </style>
 
 <body>
@@ -162,27 +180,306 @@
 	  	 	</tr>
 	  	 </table>
 
-	  	 <table class="twocol-table">
-          <tr>
+	  	 <table class="twocol-table margintop">
+           <tr>
 	  	 	<td class="col-8"><label class="table-head-font">Sl No</label></td>
 	  	 	<td class="col-65"><span class="table-head-font">Particulars</span></td>
 	  	 	<td class="col-7"><label class="table-head-font">CCR/CRS/<br>Cashier</label></td>
-	  	 	<td class="col-7"><label class="table-head-font">CRM/<br>PM</label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-head-font ">CRM/<br>PM</label></td>
 	  	 	<td class="col-7"><label class="table-head-font">HO <br>Maker</label></td>
-	  	 	<td class="col-7"><label class="table-head-font">HO <br>Checker</label></td>
-          </tr>
-          <tr>
-          	<td class="col-8"><label class="table-font">1</label></td>
-	  	 	<td class="col-65">
-	  	 		<span class="table-font">Deceased Name in Bajaj Claim form match with Death Certificate, Age / ID Proof, FIR/Post Mortem Report</span><hr class="no-padding" />
-	  	 		<span class="table-font">If name is not matching, Need Court Affidavit (mentioning all the names)</span>
+	  	 	<td class="col-7 td-bg"><label class="table-head-font">HO <br>Checker</label></td>
+           </tr>
+           <tr>
+		 	 <td class="col-8 lableslno" rowspan="2" ><label class="table-font">1</label></td>
+		 	 <td class="col-65">
+		    <span class="table-font">Deceased Name in Bajaj Claim form match with Death Certificate, Age / ID Proof, FIR/Post Mortem Report</span>
+		 	 </td>
+		 	 <td class="col-7"><label class="table-font"></label></td>
+		 	 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		 	 <td class="col-7"><label class="table-font"></label></td>
+		 	 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		   </tr>
 
+		   <tr>
+			 <td class="col-65">
+			   <span class="table-font"><strong>If name is not matching,</strong> Need Court Affidavit (mentioning all the names)</span>
+			 </td>
+			 <td class="col-7"><label class="table-font"></label></td>
+			 <td class="col-7 td-bg"><label class="table-font"></label></td>
+			 <td class="col-7"><label class="table-font"></label></td>
+			 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		   </tr>
+
+		    <tr>
+		 	 <td class="col-8 lableslno" rowspan="2" ><label class="table-font">2</label></td>
+		 	 <td class="col-65">
+		    <span class="table-font">Deceased age (as per Bajaj Claim form) match with Death Certificate, Age / ID Proof, FIR
+                 or Post Mortem Report</span>
+		 	 </td>
+		 	 <td class="col-7"><label class="table-font"></label></td>
+		 	 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		 	 <td class="col-7"><label class="table-font"></label></td>
+		 	 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		   </tr>
+
+		   <tr>
+			 <td class="col-65">
+			   <span class="table-font"><strong>If Age difference is 5 years (+ or -)</strong> Need Court Affidavit </span>
+			 </td>
+			 <td class="col-7"><label class="table-font"></label></td>
+			 <td class="col-7 td-bg"><label class="table-font"></label></td>
+			 <td class="col-7"><label class="table-font"></label></td>
+			 <td class="col-7 td-bg"><label class="table-font"></label></td>
+		   </tr>
+
+
+
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">3</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">Correct Customer ID is reflecting in Bajaj Claim form?</span>
 	  	 	</td>
 	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
 	  	 	<td class="col-7"><label class="table-font"></label></td>
-	  	 	<td class="col-7"><label class="table-font"></label></td>
-	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
           </tr>
+
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">4</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">Is Date of Death same in the following documents?<br>
+                Bajaj Claim form, Death Certificate and FIR or Post Mortem Report</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">5</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">Is this Accidental / Murder Death?</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">If above is Yes, is Post Mortem or FIR Report attached?</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">6</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">Check Seal & Sign on Death Certificate, FIR & Post Mortem Report<br>
+	  	 			<strong>(Proceed If Death Certificate is Computer generated with barcode)</strong></span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">7</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">In death certificate, is date of registration and date of issuance are equal or greater than date of death? If no, get revised death certificate</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font">8</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font"><strong>Documents attached (Mandatory)</strong><br>
+	  	 			<label class="table-font">a) Bajaj Claimant Statement (Claim Form) with only nominee signature</label></span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">b) Death certificate issued by registrar of birth & death (Form No. 6)</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">c) Post Mortem  / FIR Report  (For accident / murder Case)</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font">d) ID and Age Proof of the deceased</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+           <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65 td-bg">
+	  	 		<span class="table-font">e) Original Loan Closure Request from Nominee</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+          <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-65 td-bg">
+	  	 		<span class="table-font ">f) ECS/ACH Mandate</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+	  	 </table>
+
+	  	 <table class="twocol-table">
+	  	 	<tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-92">
+	  	 		<span class="table-font "><strong>Nominee bank account details in BLOCK letters (English only)</strong></span>
+	  	 	</td>
+	  	 	
+          </tr>
+	  	 </table>
+	  	 <table class="twocol-table">
+	  	  <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-40">
+	  	 		<span class="table-font "><strong>g) Nominee name as per a/c passbook</strong></span>
+	  	 	</td>
+	  	 	<td class="col-52"></td> 	
+          </tr>
+
+          <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-40">
+	  	 		<span class="table-font "><strong>h) Bank Account Number</strong></span>
+	  	 	</td>
+	  	 	<td class="col-52"></td>
+          </tr>
+
+          <tr>
+          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+	  	 	<td class="col-40">
+	  	 		<span class="table-font "><strong>i) Name of the Bank</strong></span>
+	  	 	</td>
+	  	 	<td class="col-52"></td>
+          </tr>
+  
+	  	 </table>
+	  	 <table class="twocol-table">
+	  	 	<tr>
+	  	 		<td class="col-8 lableslno"></td>
+	  	 		<td>j) MICR Code</td>
+	  	 		<td></td>
+	  	 		<td>k) IFSC Code</td>
+	  	 		<td></td>
+	  	 	</tr>
+	  	 </table>	
+         <table class="twocol-table">
+	  	 	 <tr>
+	          	<td class="col-8 lableslno"><label class="table-font"></label></td>
+		  	 	<td class="col-40">
+		  	 		<span class="table-font "><strong>i) Name of the Bank</strong></span>
+		  	 	</td>
+		  	 	<td class="col-52"></td>
+	          </tr>
+	  	 </table>
+
+	  	 <table class="twocol-table">
+	  	 	<tr>
+          	<td class="col-8 lableslno"><label class="table-font">9</label></td>
+	  	 	<td class="col-65">
+	  	 		<span class="table-font ">Are all Documents are clear and readable?</span>
+	  	 	</td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+	  	 	<td class="col-7"><label class="table-font"></label></td>
+	  	 	<td class="col-7 td-bg"><label class="table-font"></label></td>
+          </tr>
+	  	 </table>
+
+	  	 <div class="margintop"></div>
+
+	  	 <label class="table-font"><strong>Declaration:</strong>  I hereby confirm that all the fields in the checklist above have been checked by me.</label>
+
+	  	  <table class="twocol-table margintop">
+	  	  <tr>
+          	<td class="col-30"><label class="table-head-font"></label></td>
+	  	 	<td class="col-20"><label class="table-head-font">Employee ID</label></td>
+	  	 	<td class="col-30"><label class="table-head-font">Name</label></td>
+	  	 	<td class="col-10"><label class="table-head-font">Signature</label></td>
+	  	 	<td class="col-10"><label class="table-head-font">Date</label></td>
+          </tr>
+          <tr>
+          	<td class="col-30"><label class="table-head-font">CCR / CRS / Cashier (Maker)</label></td>
+	  	 	<td class="col-20"><label class="table-head-font">UJJ</label></td>
+	  	 	<td class="col-30"><label class="table-head-font"></label></td>
+	  	 	<td class="col-10"><label class="table-head-font"></label></td>
+	  	 	<td class="col-10"><label class="table-head-font"></label></td>
+          </tr>
+          <tr>
+          	<td class="col-30"><label class="table-head-font">CRM / PM (Checker)</label></td>
+	  	 	<td class="col-20"><label class="table-head-font">UJJ</label></td>
+	  	 	<td class="col-30"><label class="table-head-font"></label></td>
+	  	 	<td class="col-10"><label class="table-head-font"></label></td>
+	  	 	<td class="col-10"><label class="table-head-font"></label></td>
+          </tr>
+	  	 </table>
+
+	  	 <hr/>
+         	
+         <span class="table-head-font">For HO USE ONLY</span>
+	  	 <table class="twocol-table-noborder">
+	  	 	<tr>
+	  	 		<td>
+	  	 			<table class="twocol-table">
+		  	 			<tr>
+		  	 				<td class="col-40"><label class="table-head-font">Insurance Maker</label></td>
+		  	 				<td class="col-60"></td>
+		  	 			</tr>
+	  	 		   </table>
+	  	 		</td>
+
+	  	 		<td>
+	  	 			<table class="twocol-table">
+		  	 			<tr>
+		  	 				<td class="col-40"><label class="table-head-font">Insurance Checker</label></td>
+		  	 				<td class="col-60"></td>
+		  	 			</tr>
+	  	 		   </table>
+	  	 		</td>
+	  	 		
+	  	 	</tr>
+
 	  	 	
 	  	 </table>
   	 </div>
