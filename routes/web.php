1c4a6c8d5384b3316ab000b0682172442d5c95d3
Sridhar-Netiapps <sridhar@netiapps.com>
2025-07-10 20:24:13 +0530

    Ldap Login functionlities added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProcessStatusController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BranchController;
@@ -14,6 +15,12 @@ use App\Http\Controllers\UploadController;
 use App\Http\Controllers\CourierController;
 use App\Http\Controllers\InsuranceHomeController;
 
+// Route::middleware('guest')->group(function () {
+//     Route::get('login', [LoginController::class, 'insex'])->name('login');
+//     Route::post('login', [LoginController::class, 'authenticate'])->middleware('throttle:5,1'); // 5 attempts per minute
+// });
+
+// Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/reports', function () {
