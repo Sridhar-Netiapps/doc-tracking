0d987d9b4d57a939f31016ec80f9cff3a3a0b5cc
druva <druva@netiapps.com>
2025-07-18 18:51:11 +0530

    auth issue resolved , dynamic branchs , View and Edit Leads

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('insurance/create_insurance',[InsuranceHomeController::class,'create'])->name('create_insurance');
     Route::post('save_claim_details',[InsuranceHomeController::class,'store'])->name('save_claim_details');
     Route::get('insurance/view_claim_details/{id}',[InsuranceHomeController::class,'show'])->name('view_claim_details');
+    Route::get('insurance/edit_claim_details/{id}',[InsuranceHomeController::class,'edit'])->name('edit_claim_details');
+
     Route::get('insurance/download_claim_form/{id}',[InsuranceHomeController::class,'download_claim_form'])->name('download_claim_form');
     Route::get('insurance/download_checklist/{id}',[InsuranceHomeController::class,'download_checklist'])->name('download_checklist');
     Route::post('insurance/import_claim_data',[InsuranceHomeController::class,'import_claim_data'])->name('import_claim_data');
