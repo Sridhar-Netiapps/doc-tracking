58a9ccc65de465f50e86863c24023fadc626ab6c
Ragavi14 <ragavi2002l@gmail.com>
2025-07-18 18:44:55 +0530

    changes home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,7 +38,7 @@
                     <div class="value invert">{{array_sum($loan_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Total Pending</div>
+                    <div class="label">Pending Docs</div>
                     <div class="value">{{ ($loan_total[1] ?? 0) + ($loan_total[2] ?? 0) + ($loan_total[3] ?? 0) + ($loan_total[4] ?? 0) + ($loan_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
@@ -58,7 +58,7 @@
                     </ul>
                 </div>
                 <div class="listView">
-                    <div class="label">Received Documents</div>
+                    <div class="label">Received Docs</div>
                     <div class="value">{{($loan_total[5] ?? 0) + ($loan_total[7] ?? 0) + ($loan_total[8] ?? 0) + ($loan_total[9] ?? 0) + ($loan_total[10] ?? 0) + ($loan_total[11] ?? 0) }}</div>
                 </div>
             </div>
@@ -70,7 +70,7 @@
                     <div class="value invert">{{array_sum($gold_loan_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label"> Total Pending</div>
+                    <div class="label">Pending Docs</div>
                     <div class="value">{{ ($gold_loan_total[1] ?? 0) + ($gold_loan_total[2] ?? 0) + ($gold_loan_total[3] ?? 0) + ($gold_loan_total[4] ?? 0) + ($gold_loan_total[6] ?? 0)}}</div>
                 </div>
                 <div class="listView">
@@ -102,7 +102,7 @@
                     <div class="value">{{($gold_loan_total[6] ?? 0)}}</div>
                 </div> --}}
                 <div class="listView">
-                    <div class="label">Received Documents</div>
+                    <div class="label">Received Docs</div>
                     <div class="value">{{($gold_loan_total[5] ?? 0) + ($gold_loan_total[7] ?? 0) + ($gold_loan_total[8] ?? 0) + ($gold_loan_total[9] ?? 0) + ($gold_loan_total[10] ?? 0) + ($gold_loan_total[11] ?? 0)}}</div>
                 </div>
             </div>
@@ -114,7 +114,7 @@
                     <div class="value invert">{{array_sum($aof_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Total Pending</div>
+                    <div class="label">Pending Docs</div>
                     <div class="value">{{ ($aof_total[1] ?? 0) + ($aof_total[2] ?? 0) + ($aof_total[3] ?? 0) + ($aof_total[4] ?? 0) + ($aof_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
@@ -146,7 +146,7 @@
                     <div class="value">{{($aof_total[6] ?? 0)}}</div>
                 </div> --}}
                 <div class="listView">
-                    <div class="label">Received Documents</div>
+                    <div class="label">Received Docs</div>
                     <div class="value">{{($aof_total[5] ?? 0) + ($aof_total[7] ?? 0) + ($aof_total[8] ?? 0) + ($aof_total[9] ?? 0) + ($aof_total[10] ?? 0) + ($aof_total[11] ?? 0)}}</div>
                 </div>
             </div>
@@ -158,7 +158,7 @@
                     <div class="value invert">{{array_sum($dtrf_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Total Pending</div>
+                    <div class="label">Pending Docs</div>
                     <div class="value">{{ ($dtrf_total[1] ?? 0) + ($dtrf_total[2] ?? 0) + ($dtrf_total[3] ?? 0) + ($dtrf_total[4] ?? 0) + ($dtrf_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
@@ -190,7 +190,7 @@
                     <div class="value">{{($dtrf_total[6] ?? 0)}}</div>
                 </div> --}}
                 <div class="listView">
-                    <div class="label">Received Documents</div>
+                    <div class="label">Received Docs</div>
                     <div class="value">{{($dtrf_total[5] ?? 0) + ($dtrf_total[7] ?? 0) + ($dtrf_total[8] ?? 0) + ($dtrf_total[9] ?? 0) + ($dtrf_total[10] ?? 0) + ($dtrf_total[11] ?? 0)}}</div>
                 </div>
             </div>
