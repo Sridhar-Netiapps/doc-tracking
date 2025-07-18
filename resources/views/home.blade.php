08ad6a293ecbe15a694da36009be44e5b8a663d2
Ragavi14 <ragavi2002l@gmail.com>
2025-07-18 18:30:25 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,11 +38,14 @@
                     <div class="value invert">{{array_sum($loan_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Pending to Proceed</div>
+                    <div class="label">Total Pending</div>
                     <div class="value">{{ ($loan_total[1] ?? 0) + ($loan_total[2] ?? 0) + ($loan_total[3] ?? 0) + ($loan_total[4] ?? 0) + ($loan_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
                     <ul>
+                        <li>
+                            <div class="label">Pending to Proceed<span class="value">{{($loan_total[1] ?? 0)}}</span></div>
+                        </li>
                         <li>
                             <div class="label">Awaiting Checker Approval <span class="value">{{($loan_total[3] ?? 0)}}</span></div>
                         </li>
@@ -67,11 +70,14 @@
                     <div class="value invert">{{array_sum($gold_loan_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Pending to Proceed</div>
+                    <div class="label"> Total Pending</div>
                     <div class="value">{{ ($gold_loan_total[1] ?? 0) + ($gold_loan_total[2] ?? 0) + ($gold_loan_total[3] ?? 0) + ($gold_loan_total[4] ?? 0) + ($gold_loan_total[6] ?? 0)}}</div>
                 </div>
                 <div class="listView">
                     <ul>
+                        <li>
+                            <div class="label">Pending to Proceed <span class="value">{{($gold_loan_total[1] ?? 0)}}</span></div>
+                        </li>
                         <li>
                             <div class="label">Awaiting Checker Approval <span class="value">{{($gold_loan_total[3] ?? 0)}}</span></div>
                         </li>
@@ -108,11 +114,14 @@
                     <div class="value invert">{{array_sum($aof_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Pending to Proceed</div>
+                    <div class="label">Total Pending</div>
                     <div class="value">{{ ($aof_total[1] ?? 0) + ($aof_total[2] ?? 0) + ($aof_total[3] ?? 0) + ($aof_total[4] ?? 0) + ($aof_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
                     <ul>
+                        <li>
+                            <div class="label">Pending to Proceed <span class="value">{{($gold_loan_total[1] ?? 0)}}</span></div>
+                        </li>
                         <li>
                             <div class="label">Awaiting Checker Approval <span class="value">{{($aof_total[3] ?? 0)}}</span></div>
                         </li>
@@ -149,11 +158,14 @@
                     <div class="value invert">{{array_sum($dtrf_total)}}</div>
                 </div>
                 <div class="listView">
-                    <div class="label">Pending to Proceed</div>
+                    <div class="label">Total Pending</div>
                     <div class="value">{{ ($dtrf_total[1] ?? 0) + ($dtrf_total[2] ?? 0) + ($dtrf_total[3] ?? 0) + ($dtrf_total[4] ?? 0) + ($dtrf_total[6] ?? 0) }}</div>
                 </div>
                 <div class="listView">
                     <ul>
+                        <li>Transit
+                            <div class="label">Pending to Proceed <span class="value">{{($gold_loan_total[1] ?? 0)}}</span></div>
+                        </li>
                         <li>
                             <div class="label">Awaiting Checker Approval <span class="value">{{($dtrf_total[3] ?? 0)}}</span></div>
                         </li>
