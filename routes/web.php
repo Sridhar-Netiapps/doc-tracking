a98bcf2a64558bca6248c9df8675120f93b622be
druva <druva@netiapps.com>
2025-07-21 12:05:20 +0530

    debug

--- routes/web.php
+++ routes/web.php
@@ -114,6 +114,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('insurance/view_claim_details/{id}',[InsuranceHomeController::class,'show'])->name('view_claim_details');
     Route::get('insurance/edit_claim_details/{id}',[InsuranceHomeController::class,'edit'])->name('edit_claim_details');
 
+
     Route::get('insurance/download_claim_form/{id}',[InsuranceHomeController::class,'download_claim_form'])->name('download_claim_form');
     Route::get('insurance/download_checklist/{id}',[InsuranceHomeController::class,'download_checklist'])->name('download_checklist');
     Route::post('insurance/import_claim_data',[InsuranceHomeController::class,'import_claim_data'])->name('import_claim_data');
@@ -121,6 +122,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::post('save_claim_checklist',[InsuranceHomeController::class,'save_claim_checklist'])->name('save_claim_checklist');
     Route::post('save_nominee_details',[InsuranceHomeController::class,'save_nominee_details'])->name('save_nominee_details');
+
 
     Route::get('insurance/audit-logs',[InsuranceHomeController::class,'audit'])->name('audit');
     Route::get('insurance/leads-report',[InsuranceHomeController::class,'report'])->name('leads_report');
