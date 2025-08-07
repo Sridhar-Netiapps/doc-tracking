e23dd1450e303d3ca285bde195539e9b85fc442f
druva <druva@netiapps.com>
2025-08-07 18:31:40 +0530

    debug

--- app/Http/Controllers/InsuranceHomeController.php
+++ app/Http/Controllers/InsuranceHomeController.php
@@ -471,8 +471,7 @@ class InsuranceHomeController extends Controller
 
         }
 
-        session()->forget('message');
-        session()->forget('failures');
+        
         
       // print_r($formArray);die();
         return view('insurance.view',compact('data','partners','products','placeofdeath','relationship','deathcause','claimstatus','procesedby','rlStat','deceased','checklistdata','nomineedata','formArray','branch','documentdata','landingTab'));
