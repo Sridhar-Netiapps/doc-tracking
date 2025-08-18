b8d8f94bbcaa960244760febfbf0fb54ac61684c
Sridhar-Netiapps <sridhar@netiapps.com>
2025-08-18 19:34:51 +0530

    http heraders added

--- app/Http/Middleware/AddHttpHeaders.php
+++ app/Http/Middleware/AddHttpHeaders.php
@@ -18,6 +18,20 @@ class AddHttpHeaders
     public function handle($request, Closure $next): Response
     {
         $response = $next($request);
+        if($response instanceof Response){
+            $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+            $response->headers->set('Pragma', 'no-cache');
+            $response->headers->set('Expires', '0');
+            
+            $response->headers->set('Content-Security-Policy',
+                "default-src 'self'; " .
+                "img-src 'self' data:; " .// for dropdown
+                //"font-src 'self' data:; " .
+                "style-src 'self' 'nonce-wUDPhZ1Z60inspnMCukimCi'; " .  // ✅ Nonce included here for apexchart
+                "script-src 'self' 'nonce-wUDPhZ1Z60inspnMCukimCi'; " .
+                "object-src 'none';"
+            );
+         }
         return $response;
     }
 }
