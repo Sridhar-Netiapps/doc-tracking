05b789ac3f50981bdb4bf56213ff464fccd0b4bd
Sridhar-Netiapps <sridhar@netiapps.com>
2025-08-25 15:36:37 +0530

    logout issue fixed

--- app/Http/Middleware/AddHttpHeaders.php
+++ app/Http/Middleware/AddHttpHeaders.php
@@ -22,7 +22,7 @@ class AddHttpHeaders
             $response->headers->set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
             $response->headers->set('Pragma', 'no-cache');
             $response->headers->set('Expires', '0');
-            
+            $response->headers->set('X-Frame-Options', 'DENY');            
             $response->headers->set('Content-Security-Policy',
                 "default-src 'self'; " .
                 "img-src 'self' data:; " .// for dropdown
@@ -31,6 +31,12 @@ class AddHttpHeaders
                 "script-src 'self' 'nonce-wUDPhZ1Z60inspnMCukimCi'; " .
                 "object-src 'none';"
             );
+            // $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'");
+            $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+            $response->headers->set('X-Content-Type-Options', 'nosniff');
+            // $response->headers->set('X-XSS-Protection', '1; mode=block');
+            // $response->headers->set('Referrer-Policy', 'no-referrer');
+            // $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
          }
         return $response;
     }
