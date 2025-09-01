57cc1911840d61ae155be79a163f20523cf8c8a7
Sridhar-Netiapps <sridhar@netiapps.com>
2025-09-01 15:12:32 +0530

    Xss added

--- app/Http/Middleware/AddHttpHeaders.php
+++ app/Http/Middleware/AddHttpHeaders.php
@@ -34,7 +34,7 @@ class AddHttpHeaders
             // $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self'; object-src 'none'; frame-ancestors 'none'");
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
             $response->headers->set('X-Content-Type-Options', 'nosniff');
-            // $response->headers->set('X-XSS-Protection', '1; mode=block');
+            $response->headers->set('X-XSS-Protection', '1; mode=block');
             // $response->headers->set('Referrer-Policy', 'no-referrer');
             // $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
          }
