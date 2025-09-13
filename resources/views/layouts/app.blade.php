90f167386cbc46aed5a203a3e370abd581d21a63
Sridhar-Netiapps <sridhar@netiapps.com>
2025-09-13 17:36:46 +0530

    datetime issue fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    {{-- <link rel="dns-prefetch" href="//fonts.bunny.net"> --}}
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/flatpickr.min.css') }}">
