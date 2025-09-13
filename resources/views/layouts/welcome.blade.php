67fdb5dfc9e3afc4194740ed567c135d8f734b5b
Sridhar-Netiapps <sridhar@netiapps.com>
2025-09-13 14:21:05 +0530

    conflict fixed

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -1,16 +1,8 @@
-@php
-    $filters = session('filters', []);
-    $selectedTat = $filters['tat'] ?? '';
-    $selectedRegion = $filters['region'] ?? '';
-    $selectedSearchType = $filters['search_type'] ?? '';
-@endphp
-
 <div class="bg">
     <div class="container-fluid">
         <div class="row justify-content-start align-items-center">
             <div class="col-1"></div>
             <div class="col-7">
-                {{-- <p>Hi! Welcome {{ auth()->user()->first_name }} (ID: {{ auth()->user()->id }})</p> --}}
                 <p>
                     Hi! Welcome
                    <b> {{ Auth::user()->first_name }}
@@ -23,22 +15,20 @@
             </div>
             @role('ho-user|master')
             <div class="col-1">
-                {{-- <label for="search_type">Search</label> --}}
                 <select id="search_type" name="search_type" class="form-select">
                     <option value="">-- Select --</option>
                     <option value="region" {{ $selectedSearchType == 'region' ? 'selected' : '' }}>Region</option>
                     <option value="tat" {{ $selectedSearchType == 'tat' ? 'selected' : '' }}>TAT</option>
                 </select>               
             </div>
-            @endrole
-            <div class="col-1" id="dynamic-dropdown" class="d-none"></div>
-            <div class="col-1" id="reset-btn-container" class="d-none">
+            <div class="col-1" id="dynamic-dropdown"  class="d-none"></div>
+            <div class="col-1" id="reset-btn-container"  class="d-none">
                 <button type="button" id="reset-btn" class="btn btn-secondary w-100">Reset</button>
             </div>
+            @endrole
             <div class="col-1"></div>
         </div>
-        <div id="template-region" class="d-none">
-            {{-- <label>Region</label> --}}
+        <div id="template-region"  class="d-none">
             <select id="region" name="region" class="form-select">
                 <option value="">-- Region --</option>
                 <option value="South" {{ $selectedRegion == 'South' ? 'selected' : '' }}>South</option>
@@ -49,7 +39,6 @@
         </div>
         
         <div id="template-tat" class="d-none">
-            {{-- <label>TAT</label> --}}
             <select id="tat" name="tat" class="form-select">
                 <option value="">-- TAT --</option>
                 <option value="30" {{ $selectedTat == '30' ? 'selected' : '' }}>30 days</option>
@@ -57,7 +46,6 @@
             </select>
         </div>
             
-            <!-- Placeholder for results -->
             <div id="result-container"></div>
                        
     </div>
