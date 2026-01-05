a299cec104f6c46d2c29c2e328c262ecab051976
Ragavi14 <ragavi2002l@gmail.com>
2026-01-05 16:02:33 +0530

    doc-sync table styles

--- resources/views/emails/doc_sync_complete.blade.php
+++ resources/views/emails/doc_sync_complete.blade.php
@@ -1,21 +1,21 @@
 <p>Dear Branch Team,</p>
 <p>New Pending documents syncing completed in the document tracker and the count is mentioned below.</p>
-<table style="border: 2px">
-    <tr>
-        <th>MB Loan Documents</th>
-        <td>{{ $data['loan']}} </td>
+<table style="border: 2px solid black; border-collapse: collapse; text-align: left;">
+    <tr style="border-bottom: 1px solid black;">
+        <th style="border-right: 1px solid black;">MB Loan Documents</th>
+        <td style="padding: 5px">{{ $data['loan']}} </td>
     </tr>
-    <tr>
-        <th>Gold Loan Documents</th>
-        <td>{{ $data['goldloan']}} </td>
+    <tr style="border-bottom: 1px solid black;">
+        <th style="border-right: 1px solid black;">Gold Loan Documents</th>
+        <td style="padding: 5px">{{ $data['goldloan']}} </td>
     </tr>
-    <tr>
-        <th>Liabilities Documents</th>
-        <td>{{ $data['aof']}} </td>
+    <tr style="border-bottom: 1px solid black;" >
+        <th style="border-right: 1px solid black;">Liabilities Documents</th>
+        <td style="padding: 5px">{{ $data['aof']}} </td>
     </tr>
-    <tr>
-        <th>DTR Files</th>
-        <td>{{ $data['dtrf']}} </td>
+    <tr style="border-bottom: 1px solid black;">
+        <th style="border-right: 1px solid black;">DTR Files</th>
+        <td style="padding: 5px">{{ $data['dtrf']}} </td>
     </tr>
 </table>
 <p><a href={{ url('/') }}>Click Here</a></p>
